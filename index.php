da21202ca9a3988d12dfc03d68c279c9d8df28a9
seekasra <ukasra@gmail.com>
2014-08-17 00:47:44 +0430

    Add Bootstrap and basic homepage structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" meta="v 3.2.0" >
+    <title>Homepage</title>
+  </head>
+  <body class="container">
+      <h1>Welcome to company</h1><br />
+      <p>here the master texts goes</p>  
+  </body>
+</html>
